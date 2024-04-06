72a4eabdc438c5c7915e6603e99997abd20d8951
Pagnussati <127996526+Pagnussati@users.noreply.github.com>
2024-04-06 12:49:08 -0300

    Buscando produtos

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LuLoader2 } from 'react-icons/lu';
+
+import './Loading.css';
+
+function Loading() {
+  return ( 
+    <LuLoader2 className="loading" />
+  );
+}
+
+export default Loading;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import ProductCard from '../ProductCard/ProductCard.jsx';
 import fetchProducts from '../../api/fetchProducts';
+import Loading from '../Loading/Loading.jsx';
+import AppContext from '../../context/AppContext';
 
 import './Products.css';
 
 function Products() {
-  const [products, setProducts] = useState([]);
 
+  const { products, setProducts, loading, setLoading } = useContext(AppContext);
+  
   useEffect(() => {
     fetchProducts('notebook gamer').then((response) => {
       setProducts(response);
+      setLoading(false);
     });
   }, []);
 
   return(
     <section className="products container">
-      {
-        products.map((product) => <ProductCard data={product} key={product.id}/>)
-      }
+      { loading ? <Loading/> : products.map((product) => <ProductCard data={product} key={product.id}/>) }
     </section>
   );
 }
